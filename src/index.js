5e6cadd729ea7498f4e312a3bbf13c646c58ee6d
Thiago Bitencourt <thiago.mbitencourt@gmail.com>
2018-09-04 06:24:13 -0300

    Add search bar functional component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search-bar';
+
 const API_KEY = 'AIzaSyDnMi7RThnPf5Ct0mHmi7j91GPloimXFnw';
 
 // Create a new componente. 
 // This componente should produce some html
 
 const App = () => {
-    return <div>Hi!</div>
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
 }
 
 // Take this component's generated html and put it
