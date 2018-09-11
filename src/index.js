ff4373d32c3e30e25180d23547578051e7f3e2c5
Thiago Bitencourt <thiago.mbitencourt@gmail.com>
2018-09-11 07:01:20 -0300

    Create video list component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
 
 const API_KEY = 'AIzaSyDnMi7RThnPf5Ct0mHmi7j91GPloimXFnw';
 
 // Create a new componente. 
 // This componente should produce some html
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
+class App extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos}/>
+            </div>
+        )
+    }
 }
 
 // Take this component's generated html and put it
