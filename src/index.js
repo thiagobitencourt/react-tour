be1f5a7a8a179d9353cd28affc6f2c8c9ba0e689
Thiago Bitencourt <thiago.mbitencourt@gmail.com>
2018-09-16 08:15:54 -0300

    Learning states and callbacks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search-bar';
+import VideoDetail from './components/video-detail';
 import VideoList from './components/video-list';
 
 const API_KEY = 'AIzaSyDnMi7RThnPf5Ct0mHmi7j91GPloimXFnw';
@@ -14,18 +16,34 @@ class App extends Component {
     constructor(props) {
         super(props);
         
-        this.state = { videos: [] };
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
-        YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {
-            this.setState({ videos });
+       this.videoSearch('surfboard');
+    }
+
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term }, videos => {
+            this.setState({ 
+                videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
     render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 500);
+
         return (
             <div>
-                <SearchBar />
-                <VideoList videos={this.state.videos}/>
+                <SearchBar onSearchTermChange={videoSearch}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+                    videos={this.state.videos}
+                />
             </div>
         )
     }
