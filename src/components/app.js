e59d0573e4df637b81970195a2c8e72d1dc43daa
Thiago Bitencourt <thiago.mbitencourt@gmail.com>
2018-09-21 07:00:25 -0300

    Creates the search bar component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
+import SearchBar from '../containers/search_bar';
+
 export default class App extends Component {
     render() {
         return (
-            <div>Hi!!</div>
+            <div>
+                <SearchBar />
+            </div>
         )
     }
 }
---
+++ src/containers/search_bar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event) {
+        this.setState({ term: event.target.value });
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+        // We need to go and fetch weather data
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onFormSubmit.bind(this)} className="input-group">
+                <input className="form-control" 
+                    placeholder="Get a five-day forecast in your favorite cities"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
+            </form>  
+        );
+    }
+}
