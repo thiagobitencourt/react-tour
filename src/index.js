8ab6835da1973643591a37c2e91045fd176a8427
Thiago Bitencourt <thiago.mbitencourt@gmail.com>
2018-09-21 06:34:53 -0300

    Reset the repo to initial state.
    To see older commits please switch branches.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-const API_KEY = 'AIzaSyDnMi7RThnPf5Ct0mHmi7j91GPloimXFnw';
+import App from './components/app';
 
-// Create a new componente. 
-// This componente should produce some html
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-const App = () => {
-    return <div>Hi!</div>
-}
-
-// Take this component's generated html and put it
-// on the page (in the DOM)
-
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(
+    <App />
+    // <Provider store={createStoreWithMiddleware()}>
+    //     <App />
+    // </Provider>
+, document.querySelector('.container'));
